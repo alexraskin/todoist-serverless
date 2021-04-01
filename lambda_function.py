b0127fbb3d600486ca873aecc50ea9c5f2ed777c
alexraskin <alexraskin@fastmail.fm>
2021-03-31 21:05:03 -0700

    refactor

--- lambda_function.py
+++ lambda_function.py
@@ -3,28 +3,36 @@ import uuid
 import json
 import requests
 
-# set inside lambda
-api_key = os.getenv('TODOIST_API_KEY')
+
+class TodoTask:
+    def __init__(self, content: str, priority: int, due_string=None, due_lang='en',
+                 api_key=os.getenv('TODO_API_KEY')) -> None:
+        self.content = content
+        self.priority = priority
+        self.due_string = due_string
+        self.due_lang = due_lang
+        self.api_key = api_key
+        self.url = 'https://api.todoist.com/rest/v1/tasks'
+        self.headers = {
+            "Content-Type": "application/json",
+            "X-Request-Id": str(uuid.uuid4()),
+            "Authorization": f"Bearer {api_key}"
+        }
+
+    def requester(self) -> dict:
+        data = json.dumps({"content": self.content,
+                           "due_string": self.due_string,
+                           "due_lang": self.due_lang,
+                           "priority": self.priority})
+        response = requests.post(
+            self.url, data=data, headers=self.headers)
+        return {
+            "statusCode": response.status_code,
+            "statusReason": response.reason,
+            "data": [response.json()]
+        }
 
 
 def lambda_handler(event, context):
-    headers = {
-        "Content-Type": "application/json",
-        "X-Request-Id": str(uuid.uuid4()),
-        "Authorization": f"Bearer {api_key}"
-    }
-    data = json.dumps({"content": "This task was created using AWS Lambda",
-                                              "due_string": "today at 8am",
-                                              "due_lang": "en",
-                                              "priority": 4})
-    response = requests.post('https://api.todoist.com/rest/v1/tasks',
-                             data=data,
-                             headers=headers)
-    if response.status_code == 200:
-        return {
-            "statusCode": response.status_code
-        }
-    else:
-        return {
-            "statusCode": response.status_code
-        }
+    task = TodoTask("Hello from Lambda", 4).requester()
+    return task
